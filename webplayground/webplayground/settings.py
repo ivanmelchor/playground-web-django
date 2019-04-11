fa50b5bddbfe79a7df78b8d8968999c3b97560ec
followersito <45855921+followersito@users.noreply.github.com>
2019-04-11 10:47:52 -0600

    Se hizo el sitio accesible a la red local

--- webplayground/webplayground/settings.py
+++ webplayground/webplayground/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5v@l9*htdt(yr!dyg6m#6tyl13(*(&-d8t17h*2b7mm2xeh#-n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'webplayground.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bd_prueba',
+        'USER': 'playground_user',      # Usuario con permisos CRUD unicamente
+        'PASSWORD': '151097',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
